2a86322890d5cb44d9fe4d10abcdd595138902e4
Axel Delsol <axel.delsol.pro@gmail.com>
2025-03-13 15:47:20 +0100

    Force dynamic for /dashboard/applications

--- src/app/dashboard/applications/page.tsx
+++ src/app/dashboard/applications/page.tsx
@@ -2,6 +2,8 @@ import { fetchApplications } from "@/lib/applications";
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page() {
   const applications = await fetchApplications();
   return (
