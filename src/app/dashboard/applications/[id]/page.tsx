482badbd012e5d0fb159213ab29737d90ad11296
Axel Delsol <axel.delsol.pro@gmail.com>
2025-03-14 10:10:49 +0100

    Add custom 404 error page

--- src/app/dashboard/applications/[id]/page.tsx
+++ src/app/dashboard/applications/[id]/page.tsx
@@ -1,9 +1,14 @@
 import { fetchApplicationById } from "@/lib/applications";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: number }> }) {
   const params = await props.params;
   const id = params.id;
   const application = await fetchApplicationById(id);
+
+  if (!application) {
+    notFound();
+  }
 
   return (
     <h1>Candidature {application.title}!</h1>
