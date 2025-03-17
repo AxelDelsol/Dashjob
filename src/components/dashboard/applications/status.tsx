e76c81e059ae9e08227b229a1f1aee4e70f8da5c
Axel Delsol <axel.delsol.pro@gmail.com>
2025-03-17 15:59:57 +0100

    Update status component

--- src/components/dashboard/applications/status.tsx
+++ src/components/dashboard/applications/status.tsx
@@ -1,3 +1,4 @@
+import { Badge } from '@/components/ui/badge'
 import { ApplicationStatus } from '@/lib/definitions'
 import clsx from 'clsx'
 
@@ -10,16 +11,16 @@ const statusToText = new Map<ApplicationStatus, string>([
 
 export default function Status({ status }: { status: ApplicationStatus }) {
   return (
-    <div className={clsx(
-      'w-22 text-center text-xs border-2 p-1.5 rounded-lg capitalize',
+    <Badge variant="outline" className={clsx(
+      'w-22 text-center text-sm border-2 p-1.5 rounded-lg',
       {
-        'bg-gray-200 border-gray-600': status === 'accepted',
-        'bg-yellow-200 border-yellow-600': status === 'pending',
-        'bg-red-200 border-red-600': status === 'rejected',
-        'bg-green-200 border-green-600': status === 'accepted'
+        'bg-gray-200 border-gray-600 text-gray-950': status === 'accepted',
+        'bg-yellow-200 border-yellow-600 text-yellow-950': status === 'pending',
+        'bg-red-200 border-red-600 text-red-950': status === 'rejected',
+        'bg-green-200 border-green-600 text-green-950': status === 'accepted'
       }
     )}>
       {statusToText.get(status)}
-    </div>
+    </Badge>
   )
 }
